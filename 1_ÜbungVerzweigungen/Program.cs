8c3b162f3ab7420282bad6011824138e497fc805
Luan Krasnici <luan.k5@gso.schule.koeln>
2022-08-24 14:45:01 +0200

    Aufgabe 1

--- 1_ÜbungVerzweigungen/Program.cs
+++ 1_ÜbungVerzweigungen/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int x = 9, z = 6; double y = 8; //false
+Console.WriteLine(x-- == y); //true
+Console.WriteLine(x == (int)y);// True
+Console.WriteLine("{0:F1}",0.123);// 0,1
+Console.WriteLine(z / y); //0,75
+Console.WriteLine(x -= 10);//-2
+Console.WriteLine / (x-- == y & true);//false
+Console.WriteLine(!(z == 6) | fasle);//false
+Console.WriteLine((x *= -2) == z);//true
